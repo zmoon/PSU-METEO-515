fac2882aecc9f52a914855c42e9d8f35029cb804
zmoon92 <zmoon92@gmail.com>
2018-10-03 21:47:21 -0400

    Create hw3 output files

---
+++ hw3/convert.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# --------------------------------------------------------------------------------
+#
+# Convert IPython notebook to html and then html to pdf
+# 
+# description of pdf options here: http://wkhtmltopdf.org/usage/wkhtmltopdf.txt
+# 
+#
+# Zach Moon
+# --------------------------------------------------------------------------------
+
+zoomfactor=3.0  # to make it not super wide and small
+
+outdir=./nbconverted
+mkdir -p $outdir
+
+for fname in hw3.ipynb; do
+
+    fbasename=${fname%.ipynb}
+
+    #> first use nbconvert to convert to html
+    jupyter nbconvert --to html $fname 
+    jupyter nbconvert --to pdf $fname  # standard article PDF using latex
+
+    #> use wkhtmltopdf to convert the html to pdf
+    /usr/local/bin/wkhtmltopdf --page-size Letter --zoom $zoomfactor \
+        ${fbasename}.html ${fbasename}.nbstyle.pdf
+
+    #> move to out dir
+    mv ${fbasename}.html $outdir
+    mv ${fbasename}.pdf $outdir
+    mv ${fbasename}.nbstyle.pdf $outdir
+
+done
+
